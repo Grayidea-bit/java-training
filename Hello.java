a8e8119c0f4f93282fb2634705543279a2549503
Gray Tsao <gray@Grays-MacBook-Air.local>
2023-09-18 14:03:01 +0800

    0918

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+        System.out.println("New program");
+        System.out.println(20000000000L);
+        //add L : int -> long
+        System.out.println(3.14159268F);
+        //add F : double -> float(4-5)
+        //''->char ""->string  
+    }
+}
