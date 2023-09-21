8ed476ba7c7641e4ccabe637da45124885dfdaa8
Gray Tsao <gray@Grays-MacBook-Air.local>
2023-09-21 16:07:25 +0800

    0921-2

--- Hello.java
+++ Hello.java
@@ -1,10 +1,61 @@
 public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello Java");
-        System.out.println("New program");
-        System.out.println(20000000000L);
+
+	final static int MAX_STACK_SIZE = 5;
+    public static int[] arr = new int[MAX_STACK_SIZE];
+    public static int temp = 4;
+    public static void push(int theElement) 
+    {
+        //System.out.println(top());
+        int c = top();
+        arr[c] = theElement;
+        temp--;
+        //return arr;
+	}
+
+    public static boolean isEmpty() 
+    {
+        //System.out.println(arr);
+        if(arr==null)
+        {
+            System.out.println("The Stack is EMPTY!!!");   
+            return true;
+        }
+        return false;
+	}
+
+
+    public static int top() 
+    {
+        int count = -1;
+
+        for(Integer a : arr)
+        {
+            if(a!=null)
+            {
+                count++;
+            }
+            else 
+            {
+                
+                break;
+            }
+        }
+        return count-temp;
+    }   
+	    
+
+    public static void main(String args[])
+    {
+        push(1);
+		push(2);
+		push(3);
+		System.out.println(top());
+
+        //System.out.println("Hello Java");
+        //System.out.println("New program");
+        //System.out.println(20000000000L);
         //add L : int -> long
-        System.out.println(3.14159268F);
+        //System.out.println(3.14159268F);
         //add F : double -> float(4-5)
         //''->char ""->string  
     }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+    public static int[] arr = {1,5,4,8};
